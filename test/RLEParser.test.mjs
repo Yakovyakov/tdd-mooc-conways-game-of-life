fcf5e4b1f956e8c8a58fc588f7d98cdffb19d16a
Yakovyakov <yyak0423@gmail.com>
2025-04-28 16:53:27 -0400

    t RLEParser, split file in header and pattern

---
+++ test/RLEParser.test.mjs
@@ -0,0 +1,16 @@
+import { expect } from "chai";
+import { describe, test } from "vitest";
+
+
+describe('RLEParser', () => {
+  describe('RLE to Grid', () => {
+    test('should split file in header and pattern', () => {
+      const rleString = 'x = 3, y = 2\nbo$2o!';
+
+      const rleParser = new RLEParser(rleString);
+
+      expect(rleParser.header).to.equal('x = 3, y = 2');
+      expect(rleParser.pattern).to.equal('bo$2o!');      
+    });
+  });
+});
