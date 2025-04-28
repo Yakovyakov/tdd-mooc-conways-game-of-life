1fa54df3fd09faf1590e0528c9317bd2590e3cbc
Yakovyakov <yyak0423@gmail.com>
2025-04-28 15:53:20 -0400

    t CELL_STATES, properties

--- test/CellStates.test.mjs
+++ test/CellStates.test.mjs
@@ -1,10 +1,16 @@
 import { expect } from "chai";
 import { describe, test } from "vitest";
 import { CELL_STATES } from "../src/Cell.mjs";
+
 describe('CELL_STATES', () => {
-  test('should be a frozen object', () => {
+  test('should be a frozen object with right property(ALIVE or DEAD)', () => {
     expect(CELL_STATES).to.be.an('object');
     expect(CELL_STATES).to.be.frozen;
+
+    expect(CELL_STATES).to.have.keys('ALIVE', 'DEAD');
+    expect(CELL_STATES.ALIVE.description).to.equal('alive');
+    expect(CELL_STATES.DEAD.description).to.equal('dead');
+    
   });
 
 });
