d949342a931717eced2d6bf49a7b75fe27af0870
Yakovyakov <yyak0423@gmail.com>
2025-04-28 15:50:59 -0400

    r Add CELL_STATES

---
+++ src/Cell.mjs
@@ -0,0 +1,2 @@
+
+export const CELL_STATES = Object.freeze({});
--- test/CellStates.test.mjs
+++ test/CellStates.test.mjs
@@ -1,6 +1,6 @@
 import { expect } from "chai";
 import { describe, test } from "vitest";
-
+import { CELL_STATES } from "../src/Cell.mjs";
 describe('CELL_STATES', () => {
   test('should be a frozen object', () => {
     expect(CELL_STATES).to.be.an('object');
