d2149ac3b81db76ae9c0bf1b5a40256e207304ef
Yakovyakov <yyak0423@gmail.com>
2025-04-30 07:38:32 -0400

    t RLEParser ignores comments and empty lines

--- test/RLEParser.test.mjs
+++ test/RLEParser.test.mjs
@@ -21,5 +21,27 @@ describe('RLEParser', () => {
       const rleString = 'x = 3, x = 2\nbo$2o!';
       expect(() => new RLEParser(rleString)).to.throw(Error, 'Invalid header');
     });
+
+    test('should ignore comments, and empty lines', () => {
+      const rleString = `
+        #N Glider
+        #O Richard K. Guy
+        #C The smallest, most common, and first discovered spaceship. Diagonal, has period 4 and speed c/4.
+        #C www.conwaylife.com/wiki/index.php?title=Glider
+
+        x = 3, y = 3, rule = B3/S23
+
+        #C another comment
+
+
+        bob$2bo$3o!
+
+
+      `;
+
+      const rleParser = new RLEParser(rleString);
+      expect(rleParser.header).to.equal('x = 3, y = 3, rule = B3/S23');
+      expect(rleParser.pattern).to.equal('bob$2bo$3o!');
+    });
   });
 });
