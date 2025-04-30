c50470da5413b9ee20365cad8aa25eafaeb57d2e
Yakovyakov <yyak0423@gmail.com>
2025-04-30 13:11:19 -0400

    t integration

--- test/e2e.test.mjs
+++ test/e2e.test.mjs
@@ -1,3 +1,4 @@
+import fs from 'fs';
 import { expect } from "chai";
 import { afterAll, beforeAll, describe, test } from "vitest";
 import { runGameOfLife } from "../src/index.mjs";
@@ -6,17 +7,20 @@ import { runGameOfLife } from "../src/index.mjs";
 describe('e2e', () => {
   const inputFile = 'test.rle';
   const generations = 4;
+  const testRLE = 'x =2, y = 2\n2o$2o!'
 
   beforeAll(() => {
+    fs.writeFileSync(inputFile, testRLE);
 
   });
 
   afterAll(() => {
+    fs.unlinkSync(inputFile)
 
   });
-  test('Walking skeleton', () => {
+  test('Integration test', () => {
     const result  = runGameOfLife(inputFile, generations);
-    expect(result).to.equal('test.rle 4');
+    expect(result).to.equal('x = 2, y = 2\n2o$2o!');
 
   });
 });
