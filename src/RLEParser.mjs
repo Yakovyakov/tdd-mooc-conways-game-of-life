37c1aa454b862c8099fb760a9e2682436cd2097e
Yakovyakov <yyak0423@gmail.com>
2025-04-30 08:11:39 -0400

    r RLEParser, add parseToGrid, non repeat tags

--- src/RLEParser.mjs
+++ src/RLEParser.mjs
@@ -1,3 +1,5 @@
+import { CELL_STATES } from "./Cell.mjs";
+import { Grid } from "./Grid.mjs";
 
 export class RLEParser {
   constructor(rleString) {
@@ -28,5 +30,43 @@ export class RLEParser {
     this.header = headerLine;
     this.pattern = lines.slice(lines.indexOf(headerLine) + 1).join('');
 
+  }
+
+  parseToGrid() {
+    const grid = new Grid(this.width, this.height);
+
+    const patternMatch = this.pattern.match(/([bo$!]+)/i);
+
+    if (!patternMatch) {
+      return grid;
+    }
+
+    const pattern = patternMatch[0];
+    let row = 0;
+    let col = 0;
+
+    for (const char of pattern) {
+      if (char === '!') {
+        break;
+      }
+
+      if (char === '$') {
+        row++;
+        col = 0;
+        continue;
+      }
+
+      if (/[bo]/.test(char)) {
+
+        const state = char === 'b' ? CELL_STATES.DEAD : CELL_STATES.ALIVE;
+
+        if (row < this.height && col < this.width) {
+          grid.setCellStateAt(row, col, state);
+        }
+        col++;
+      }
+
+    }
+    return grid;
   }
 }
