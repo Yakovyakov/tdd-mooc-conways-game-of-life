d4298ed07d7c80f158e05f2eca5841aee8a23b8c
Yakovyakov <yyak0423@gmail.com>
2025-04-30 07:44:55 -0400

    t RLEParser multiple lines in pattern are join into one

--- test/RLEParser.test.mjs
+++ test/RLEParser.test.mjs
@@ -43,5 +43,23 @@ describe('RLEParser', () => {
       expect(rleParser.header).to.equal('x = 3, y = 3, rule = B3/S23');
       expect(rleParser.pattern).to.equal('bob$2bo$3o!');
     });
+
+    test('pattern, multiple lines are joined into one', () => {
+      const rleString = `
+        #N Gosper glider gun
+        #O Bill Gosper
+        #C A true period 30 glider gun.
+        #C The first known gun and the first known finite pattern with unbounded growth.
+        #C www.conwaylife.com/wiki/index.php?title=Gosper_glider_gun
+        x = 36, y = 9, rule = B3/S23
+        24bo11b$22bobo11b$12b2o6b2o12b2o$11bo3bo4b2o12b2o$2o8bo5bo3b2o14b$2o8b
+        o3bob2o4bobo11b$10bo5bo7bo11b$11bo3bo20b$12b2o!
+      `;
+
+      const rleParser = new RLEParser(rleString);
+      expect(rleParser.header).to.equal('x = 36, y = 9, rule = B3/S23');
+      expect(rleParser.pattern).to.equal('24bo11b$22bobo11b$12b2o6b2o12b2o$11bo3bo4b2o12b2o$2o8bo5bo3b2o14b$2o8bo3bob2o4bobo11b$10bo5bo7bo11b$11bo3bo20b$12b2o!');
+
+    });
   });
 });
